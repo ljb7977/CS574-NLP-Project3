33ee0496f4752b1686683dd160cb5840c39ee2a9
Lee Ju Been <ljb7977@gmail.com>
2018-05-28 14:48:30 +0900

    update

---
+++ SRL/glove/main.py
@@ -0,0 +1,33 @@
+from sklearn.feature_extraction.text import CountVectorizer
+import operator, pickle
+import glove
+
+with open("../UCorpus-DP_SR/BGAD0038_new.txt", encoding="utf-8") as input_file:
+	lines = input_file.readlines()
+	vectorizer = CountVectorizer(min_df=10, ngram_range=(1,1))
+	X = vectorizer.fit_transform(corpus)
+	Xc = X.T * X             # co-occurrence matrix
+	Xc.setdiag(0)			 # 대각성분을 0으로
+	result = Xc.toarray()    # array로 변환
+	dic = {}
+	for idx1, word1 in enumerate(result):
+		tmpdic = {}
+		for idx2, word2 in enumerate(word1):
+			if word2 > 0:
+				tmpdic[idx2] = word2
+		dic[idx1] = tmpdic
+
+	vocab = sorted(vectorizer.vocabulary_.items(), key=operator.itemgetter(1))
+	vocab = [word[0] for word in vocab]
+
+	model = glove.Glove(dic, d=100, alpha=0.75, x_max=100.0)
+	for epoch in range(25):
+		err = model.train(batch_size=200, workers=4)
+		print("epoch %d, error %.3f" % (epoch, err), flush=True)
+
+	# 단어벡터 추출
+	wordvectors = model.W
+
+	# 저장
+	with open('glove', 'wb') as f:
+		pickle.dump([vocab,wordvectors],f)
--- syntax/maltparser-1.9.2/eval.sh
+++ syntax/maltparser-1.9.2/eval.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
-
-java -jar maltparser-1.9.2.jar -c kaist -i data/kaist_dev.conll -o dev_out.conll -m parse
-echo Parsing Done
-head -n -1 dev_out.conll > temp.txt
-mv temp.txt dev_out.conll
-java -jar evaluator.jar dev_out.conll data/kaist_dev.conll
+for feature in ./features/*
+do
+	path=`echo $feature`
+	name=`echo $feature | sed -e 's/.\/features\///g' | sed -e 's/.xml//g'`
+	echo $name
+	echo $path
+	java -jar maltparser-1.9.2.jar -c $name -i data/kaist_train.conll -f finalOptionsFile.xml -F $path -m learn
+	echo $name training done
+	java -jar maltparser-1.9.2.jar -c $name -i data/kaist_dev.conll -o dev_out.conll -f finalOptionsFile.xml -F $path -m parse
+	echo $name parsing done
+	head -n -1 dev_out.conll > temp.txt
+	mv temp.txt dev_out.conll
+	java -jar evaluator.jar dev_out.conll data/kaist_dev.conll
+	echo '\n'
+done
